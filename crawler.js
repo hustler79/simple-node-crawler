62ddc4112f942aaa929a7b1b23e825c52f41567a
Martin R <hustler@op.pl>
2017-06-03 19:17:43 +0200

    Create crawler.js

---
+++ crawler.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const request = require('request-promise-native');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const maxRequest = 20;
+const secureSleepTime = 120000; // 2min
+let requestCounter = 0;
+
+const searchPhrase = <SEARCH_PHRASE>;
+const startUrl = "https://panoramafirm.pl/"+ searchPhrase;
+
+const makeRequest = function(url, pause = false) {
+    if (pause === true) {
+        requestCounter = 0;
+    } else {
+        requestCounter++;
+    }
+
+    request({
+        method: 'GET',
+        uri: url,
+        resolveWithFullResponse: true
+    }).then(function (response) {
+        if (response.statusCode === 200) {
+            return processBody(response.body);
+        } else {
+            console.log('Other status: ', response.statusCode);
+            process.exit(0);
+        }
+    }).then(function (dataObj) {
+        writeToFile(dataObj);
+    }).catch(function (error) {
+        console.log('Error: ', error);
+    });
+};
+
+function processBody(body) {
+    const $ = cheerio.load(body, {
+        normalizeWhitespace: true
+    });
+
+    const nextUrl = $('head').find('link[rel=next]').attr('href');
+    const dataArray = [];
+
+    $('#serpContent ul li.business-card').each(function (index, element) {
+
+        const companyName = $(element).find('.business-card-title').text();
+        const emailElement = $(element).find('.business-card-bottom-bar li').eq(2);
+        const emailString = $(emailElement).find('a').attr('href');
+        const obj = {
+            key: normalizeString(companyName),
+            value: getEmail(emailString)
+        };
+        dataArray.push(obj);
+
+    });
+
+    if (nextUrl != null) {
+        console.log('Next site to parse: ', nextUrl);
+
+        if (maxRequest === requestCounter) {
+            console.log('Sleep for: ', secureSleepTime, 'ms');
+            setTimeout(makeRequest, secureSleepTime, nextUrl, true);
+        } else {
+            setTimeout(makeRequest, 5000, nextUrl);
+        }
+
+    } else {
+        console.log('No sites to parse.');
+    }
+    return dataArray;
+};
+
+function normalizeString(rawString) {
+    return rawString.trim()
+        .replace(/(\r\n|\n|\r)/gm,"")
+        .replace(/\s\s+/g, ' ')
+        .replace(/"/g, "'");
+}
+
+function getEmail(hrefText) {
+    const email = /^mailto:(.*)$/.exec(hrefText);
+    if (email[1] !== "") {
+        return email[1];
+    }
+    return 'Empty email address';
+}
+
+function writeToFile(dataObj) {
+    for (let i = 0; i < dataObj.length; i++) {
+        fs.appendFileSync('./result_data.csv', `"${dataObj[i].key}","${dataObj[i].value}"\n`);
+    }
+}
+
+makeRequest(startUrl);
